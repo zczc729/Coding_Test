9d1f9d5a1b46c8ea98bff282867a0e577353b274
사과톡톡톡 <102545749+zczc729@users.noreply.github.com>
2024-05-30 10:19:36 +0900

    Add files via upload

--- quiz/lv3/앞뒤가_같은_수.py
+++ quiz/lv3/앞뒤가_같은_수.py
@@ -21,7 +21,7 @@ n은 1부터 시작하며 크기에는 제한이 없다.
 예 5) 30000 => 200000002
 예 6) 1000000 => 90000000009
 """
-n = 900
+n = 30000
 num = 0
 inc_num = 0
 is_break = False
@@ -30,26 +30,26 @@ for i in range(n-1):
     is_num = False
     while True:
         inc_num += 1
-        print("i : ", i)
-        print("inc_num : ", inc_num)
+        # print("i : ", i)
+        # print("inc_num : ", inc_num)
         ls = list(str(inc_num))
 
         if len(ls) < 2:
             num = inc_num
-            print("1. num : ", num)
+            # print("1. num : ", num)
             break
         else:
             for j in range(int(len(ls) / 2)):
                 if ls[j] == ls[(j + 1) * -1]:
-                    print("2. num : ", num)
+                    # print("2. num : ", num)
                     num = inc_num
                     is_break = True
-                    print("2. T/F : ", is_break)
                 else:
                     is_break = False
+                    break
             if is_break:
                 break
-    print("3. num : ", num)
+    # print("3. num : ", num)
 
 
 print(num)
